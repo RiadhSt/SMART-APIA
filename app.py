2430e1fca91b5e8dac457e0b70d7b3a1cd4d28ca
RiadhSt <riadhkouki21@gmail.com>
2026-05-13 16:28:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -101,8 +101,8 @@ if not st.session_state.messages:
             <li>لا يتم تسجيل أو تخزين أو استعمال محتوى المحادثة لتدريب نماذج الذكاء الاصطناعي.</li>
             <li>لا يتم اعتماد محتوى هذه الدردشة كقرار إداري أو التزام رسمي للوكالة.</li>
         </ul>
-        <br>
-<span class="highlight-yellow">كيف يمكنني مساعدتكم اليوم؟</span>    </div>
+       
+            <span class="highlight-yellow">          كيف يمكنني مساعدتكم اليوم؟</span>    </div>
     """, unsafe_allow_html=True)
 
 # عرض المحادثة
