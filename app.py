87ce9c6418426219dfc3f836736c4280557ec148
RiadhSt <riadhkouki21@gmail.com>
2026-05-13 13:39:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ st.markdown(f"""
     </style>
     """, unsafe_allow_html=True)
 
-st.title("🤖 مساعد APIA الذكي")
+#st.title("🤖 مساعد APIA الذكي")
 
 # --- 3. إدارة الملفات (Python RAG Logic) ---
 @st.cache_resource
